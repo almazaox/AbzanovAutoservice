04425d0f1a3faabd2561bdbaecf7ebb1ca3ff7e1
Алмаз(ноутбук) <abzanov2005@gmail.com>
2023-11-02 16:01:15 +0300

    Лаба 6

--- AbzanovAutoservice/AddEditPage.xaml.cs
+++ AbzanovAutoservice/AddEditPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AbzanovAutoservice.Resources;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,46 @@ namespace AbzanovAutoservice
     /// </summary>
     public partial class AddEditPage : Page
     {
-        public AddEditPage()
+        private Service _currentServise = new Service();
+
+        public AddEditPage(Service SelectedService)
         {
             InitializeComponent();
+            if (SelectedService != null)
+                _currentServise = SelectedService;
+            DataContext = _currentServise;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentServise.Title))
+                errors.AppendLine("Укажите название услуги");
+            if (_currentServise.Cost == 0)
+                errors.AppendLine("Укажите стоимость услуги");
+            if (_currentServise.Discount == null)
+                errors.AppendLine("Укажите скидку");
+            if (string.IsNullOrWhiteSpace(_currentServise.DurationInSeconds))
+                errors.AppendLine("Укажите длительность услуги");
+            if(errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentServise.ID == 0)
+                Abzanov_AutoserviceEntities.GetContext().Service.Add(_currentServise);
+            try
+            {
+                Abzanov_AutoserviceEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+
         }
     }
 }
--- AbzanovAutoservice/ServicePage.xaml.cs
+++ AbzanovAutoservice/ServicePage.xaml.cs
@@ -67,10 +67,7 @@ namespace AbzanovAutoservice
             }
             ServiceListView.ItemsSource = currentServices;
         }
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Manager.MainFrame.Navigate(new AddEditPage());
-        }
+      
 
         private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -95,12 +92,21 @@ namespace AbzanovAutoservice
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-            Manager.MainFrame.Navigate(new AddEditPage());
+            Manager.MainFrame.Navigate(new AddEditPage(null));
         }
 
         private void EditButton_Click(object sender, RoutedEventArgs e)
         {
-            Manager.MainFrame.Navigate(new AddEditPage());
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Service));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility == Visibility.Visible)
+            {
+                Abzanov_AutoserviceEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                ServiceListView.ItemsSource = Abzanov_AutoserviceEntities.GetContext().Service.ToList();
+            }
         }
     }
 }
